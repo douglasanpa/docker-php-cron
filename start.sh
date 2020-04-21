037176121cbef25e8da14d3ee6258a6a52f14ce2
Douglas Andrade <douglasanpa@gmail.com>
2020-04-21 01:08:50 +0000

    Fixes

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 chown root:root /etc/crontabs/root
 chown xfs:xfs /var/tmp/nginx -R
+chown xfs:xfs /var/tmp/nginx -R
+chown xfs:xfs /var/lib/php* -R
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
